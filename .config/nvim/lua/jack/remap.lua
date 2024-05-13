7e0f857d7e6dfc9259f9466f755ca8887eaaab90
SpaceWaffles <jackhartwig7@gmail.com>
2024-05-12 22:16:48 -0500

    changed the jump page to h and j fix conflict

--- .config/nvim/lua/jack/remap.lua
+++ .config/nvim/lua/jack/remap.lua
@@ -5,8 +5,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- half page jumping (Need to remap)
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- half page jumping
+vim.keymap.set("n", "<C-k>", "<C-d>zz") -- half page jumping (Need to remap)
+vim.keymap.set("n", "<C-j>", "<C-u>zz") -- half page jumping
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -27,8 +27,8 @@ vim.keymap.set("n", "<C-t>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+--vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+--vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
